29062acb3d1d27a7fd49f2ad1d33c8eb53ce22e5
Jinyoung Jang <jyjang@uengine.org>
2022-08-08 19:51:44 +0900

    commit msg

---
+++ frontend/src/components/메뉴삭제Command.vue
@@ -0,0 +1,60 @@
+<template>
+
+    <v-card outlined>
+        <v-card-title>
+            메뉴삭제
+        </v-card-title>
+
+        <v-card-text>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="메뉴삭제"
+            >
+                메뉴삭제
+            </v-btn>
+            
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="close"
+            >
+                Close
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+
+</template>
+
+<script>
+   
+    export default {
+        name: '메뉴삭제Command',
+        components:{},
+        props: {},
+        data: () => ({
+            editMode: true,
+            value: {},
+        }),
+        created() {
+        },
+        watch: {
+        },
+        methods: {
+            메뉴삭제() {
+                this.$emit('메뉴삭제', this.value);
+            },
+            close() {
+                this.$emit('closeDialog');
+            },
+            change() {
+                this.$emit('input', this.value);
+            },
+        }
+    }
+</script>
+
---
+++ store/src/main/java/fooddelivery/external/Order.java
@@ -0,0 +1,19 @@
+package fooddelivery.external;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class Order {
+
+    private Long id;
+    private String foodId;
+    private String customerId;
+    private String preference;
+    private String options;
+    private Object address;
+    private String status;
+    private Object money;
+    // keep
+
+}
